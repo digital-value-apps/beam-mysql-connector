0dd87fc0c12fa30186a3d498370de498dec6c0d8
Lenny COLIN <lennycolin@hotmail.fr>
2021-12-20 18:11:00 +0100

    feat: add replace to writetomysql

--- beam_mysql/connector/io.py
+++ beam_mysql/connector/io.py
@@ -62,6 +62,7 @@ class WriteToMySQL(PTransform):
         password: Union[str, ValueProvider],
         port: Union[int, ValueProvider] = 3306,
         batch_size: int = 1000,
+        replace: bool = False,
     ):
         super().__init__()
         self._host = host
@@ -71,6 +72,7 @@ class WriteToMySQL(PTransform):
         self._password = password
         self._port = port
         self._batch_size = batch_size
+        self._replace = replace
 
     def expand(self, pcoll: PCollection) -> PCollection:
         return pcoll | beam.ParDo(
@@ -82,6 +84,7 @@ class WriteToMySQL(PTransform):
                 self._password,
                 self._port,
                 self._batch_size,
+                self._replace,
             )
         )
 
@@ -98,6 +101,7 @@ class _WriteToMySQLFn(beam.DoFn):
         password: Union[str, ValueProvider],
         port: Union[int, ValueProvider],
         batch_size: int,
+        replace: bool,
     ):
         super().__init__()
         self._host = host
@@ -107,6 +111,7 @@ class _WriteToMySQLFn(beam.DoFn):
         self._password = password
         self._port = port
         self._batch_size = batch_size
+        self._replace = replace
 
         self._config = {
             "host": self._host,
@@ -155,7 +160,8 @@ class _WriteToMySQLFn(beam.DoFn):
                 self._columns_and_values[column_str].clear()
 
     def _build_query(self, column_str, values_str):
-        return f"INSERT INTO {self._config['database']}.{self._table}({column_str}) VALUES {','.join(values_str)};"
+        statement = "REPLACE" if self._replace else "INSERT"
+        return f"{statement} INTO {self._config['database']}.{self._table}({column_str}) VALUES {','.join(values_str)};"
 
     def _build_value(self):
         for k, v in self._config.items():
